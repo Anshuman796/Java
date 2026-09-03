7d25bac877ce46036c3e46e9f1a5373d6df2cb43
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 14:55:50 -0700

    Create BreakStatement.java
    Added BreakStatement.java

---
+++ Loops/BreakStatement.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class BreakStatement {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            if(i==4){
+                break;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
