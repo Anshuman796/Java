bf94316eda95ffa404f08dea53719400232cae43
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 06:11:57 -0700

    Create DecrementOperator.java
    Added DecrementOperator.java

---
+++ Basics/DecrementOperator.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class DecrementOperator {
+    public static void main(String [] args){
+        int x=10;
+        x--;                 // Post Decrement Operator
+        System.out.println(x);
+
+        int y=20;
+        --y;                 // Pre Decrement Operator
+        System.out.println(y);
+    }
+}
