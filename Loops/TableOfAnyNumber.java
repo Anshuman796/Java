4d1413cea1b7db6ede4a0e93ff84cd06c4e19923
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 14:42:06 -0700

    Create TableOfAnyNumber.java
    Added Table.java

---
+++ Loops/TableOfAnyNumber.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class TableOfAnyNumber {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int n= sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+
+        sc.close();
+    }  
+}
