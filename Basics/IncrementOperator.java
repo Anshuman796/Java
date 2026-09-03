e2560c8054bbc854c249dcb503a23447f01b619f
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 06:09:35 -0700

    Create IncrementOperator.java
    Added IncrementOperator.java

---
+++ Basics/IncrementOperator.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class IncrementOperator {
+    public static void main(String[] args){
+        int x=10;
+        x++;                 // Post Increment Operator
+        System.out.println(x);
+
+        int y=20;
+        ++y;                 // Pre Increment Operator
+        System.out.println(y);
+
+    }
+}
