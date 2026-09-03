0ca85e6ff2f3da17f576f3683fa549ecaa24f886
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 06:45:22 -0700

    Create BytesOfDataTypes.java
    Added BytesOfDataTypes.java

---
+++ Basics/BytesOfDataTypes.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class BytesOfDataTypes {
+    public static void main(String[] args){
+        System.out.println(Byte.BYTES);
+        System.out.println(Character.BYTES);
+        System.out.println(Short.BYTES);
+        System.out.println(Integer.BYTES);
+        System.out.println(Float.BYTES);
+        System.out.println(Double.BYTES);
+        System.out.println(Long.BYTES);
+        
+    }
+}
