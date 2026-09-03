d1f6a22e9e067d786ddaace03ff7994ebb9354a7
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 07:01:20 -0700

    Create AgeCriteria.java
    Added AgeCriteria.java

---
+++ Conditionals/AgeCriteria.java
@@ -0,0 +1,21 @@
+package Conditionals;
+
+import java.util.Scanner;
+
+public class AgeCriteria {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter Your Age: ");
+        int age= sc.nextInt();
+
+        if(age>=18){
+            System.out.println("Eligible");
+        }
+        else{
+            System.out.println("Not Eligible");
+        }
+
+
+        sc.close();
+    }
+}
