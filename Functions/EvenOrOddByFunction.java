32c6d6849efd6d1db7bf59d84a7b092c76e8d930
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 19:18:16 -0700

    Create EvenOrOddByFunction.java
    Added Function.java

---
+++ Functions/EvenOrOddByFunction.java
@@ -0,0 +1,19 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class EvenOrOddByFunction {
+
+    public static void EvenOrOdd(int n){
+        if(n%2==0) System.out.print("Even");
+        else System.out.print("Odd");
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int n= sc.nextInt();
+        EvenOrOdd(n);
+
+        sc.close();
+    }    
+}
