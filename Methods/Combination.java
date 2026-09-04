881a2b08a3ceb09405838cb19232e5aa0184e696
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-04 06:44:48 -0700

    Create Combination.java
    Added combination.java

---
+++ Methods/Combination.java
@@ -0,0 +1,27 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class Combination{
+
+    public static int factorial(int x){
+        int fact=1;
+        for(int i=1;i<=x;i++){
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of n: ");
+        int n = sc.nextInt();
+        System.out.print("Enter Value of r: ");
+        int r = sc.nextInt();
+
+        int ncr= factorial(n)/(factorial(n-r)*factorial(r));
+
+        System.out.print(ncr);
+
+        sc.close();
+    }
+}
