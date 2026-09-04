4af953a5be55008701b773af8a3de1e57ac8c6a0
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-04 06:40:31 -0700

    Create BuiltInFunction.java
    Added BuiltInFunction.java

---
+++ Functions/BuiltInFunction.java
@@ -0,0 +1,15 @@
+package Functions;
+
+public class BuiltInFunction {
+    public static void main(String[] args){
+        
+        System.out.println(Math.min(1,2));
+        System.out.println(Math.max(2,1)); 
+        
+        System.out.println(Math.pow(2,2));
+        System.out.println(Math.sqrt(64));
+
+        System.out.println(Math.cbrt(243));
+        System.out.println(Math.abs(-4));
+    }
+}
