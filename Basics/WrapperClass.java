3bd3eb355ce8b8cc30e59919d1979318f2af3050
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-04 07:08:49 -0700

    Create WrapperClass.java
    Added WrapperClass.java

---
+++ Basics/WrapperClass.java
@@ -0,0 +1,11 @@
+package Basics;
+
+public class WrapperClass {
+    public static void main(String[] args){
+
+        int x = 10;              // primitive data type
+        Integer y = x;           // Wrapper Class
+        System.out.print(y instanceof Integer);
+
+    }
+}
