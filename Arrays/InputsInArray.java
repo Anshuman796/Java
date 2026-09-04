81eb5666c344471b082f98189cfc54b65ce7613a
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-04 08:13:44 -0700

    Create InputsInArray.java
    Added Array.java

---
+++ Arrays/InputsInArray.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class InputsInArray {
+   public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Your Array Size: ");
+        int n= sc.nextInt();
+
+        int [] arr = new int[n];
+        System.out.println("Enter Your Array Element: ");
+        for(int i=0;i<=n-1;i++){
+            arr[i]=sc.nextInt();
+
+        }
+
+        for(int i=0;i<=n-1;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        sc.close();
+
+    } 
+}
