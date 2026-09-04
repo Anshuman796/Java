d47aa574fdd28596ab9e1d67bc44250d556df60f
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-03 19:06:45 -0700

    Create SumOfTwoNumberByFunction.java
    Added SumByFunction.java

---
+++ Functions/SumOfTwoNumberByFunction.java
@@ -0,0 +1,20 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class SumOfTwoNumberByFunction {
+
+    public static int sum(int x, int y){
+        return x+y;
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int result=sum(2,2);
+
+        System.out.print(result);
+
+
+        sc.close();
+    }
+}
