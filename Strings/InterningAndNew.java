14d52c7a673b8104b6d8b0cd98df7880a5432d69
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-06 07:33:42 -0700

    Create InterningAndNew.java
    Added Strinh.java

---
+++ Strings/InterningAndNew.java
@@ -0,0 +1,11 @@
+package Strings;
+
+public class InterningAndNew {
+    public static void main(String[] args){
+        String str = "Hello";
+        String gtr = "Hello";   // Here both str and gtr point the same hello in heap memory and this process knowns as interning and it save memory.
+        
+        String x = new String("Hello");   // interning problem is solve by using new keywords
+    
+    }
+}
