8915dcac9bc00aebc1e6e5e0395de791366cfd8d
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-06 07:39:41 -0700

    Update InterningAndNew.java
    Added String.java

--- Strings/InterningAndNew.java
+++ Strings/InterningAndNew.java
@@ -3,7 +3,7 @@ package Strings;
 public class InterningAndNew {
     public static void main(String[] args){
         String str = "Hello";
-        String gtr = "Hello";   // Here both str and gtr point the same hello in heap memory and this process knowns as interning and it save memory.
+        String gtr = "Hello";   // Here both str and gtr point the same content(hello) in heap memory and this process knowns as interning and it save memory.
         
         String x = new String("Hello");   // interning problem is solve by using new keywords
     
