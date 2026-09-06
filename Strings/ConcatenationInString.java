9a0c7a49e1c787cedf6b6a18a2fda24fdb4e1fca
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-06 07:06:12 -0700

    Create ConcatenationInString.java
    Added String.java

---
+++ Strings/ConcatenationInString.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class ConcatenationInString {
+    public static void main(String[] args){
+        String str = "Anshuman Singh";
+        str+=10;
+        System.out.println(str);
+
+        String gtr = "Amit Singh";
+        gtr+='A';
+        System.out.println(gtr);
+
+        String ftr = "ABCD";
+        ftr+="EFGH";
+        System.out.println(ftr);
+    }
+}
