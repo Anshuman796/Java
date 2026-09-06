6260005722c47af2459fc752cc472b4693b6dcdd
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-06 03:51:52 -0700

    Create GetterAndSetter.java
    Added oops.java

---
+++ OOPS/GetterAndSetter.java
@@ -0,0 +1,17 @@
+package OOPS;
+
+public class GetterAndSetter {
+    
+    private String Name;
+    int Rollno;
+    double percentage;
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String n){
+        Name=n;
+    }
+
+}
