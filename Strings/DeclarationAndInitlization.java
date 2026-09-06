912c2486aa3df960b6fc8b1d0da24ec1a4ee812d
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-06 06:47:19 -0700

    Create DeclarationAndInitlization.java
    Added String.java

---
+++ Strings/DeclarationAndInitlization.java
@@ -0,0 +1,15 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class DeclarationAndInitlization{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String str = "Anshuman Singh";            // Declaration And inilization
+        System.out.println(str);
+
+
+        sc.close();
+    }
+}
