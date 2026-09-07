40bf0a668011136ea464680455f72300454b667e
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-06 18:36:44 -0700

    Create ThrowsKeyword.java
    Added Exception.java

---
+++ ExceptionHandling/ThrowsKeyword.java
@@ -0,0 +1,20 @@
+package ExceptionHandling;
+
+public class ThrowsKeyword {
+    public  static int Change(int [] brr) throws ArrayIndexOutOfBoundsException{
+        return brr[8]=100;
+    }
+    public static void main(String[] args){
+        int [] arr = new int[5];
+
+        System.out.println(arr[2]);
+        
+        try{
+            Change(arr);
+            System.out.print(arr[8]);
+        }
+        catch(Exception e){
+            System.out.print(e.getMessage());
+        }
+    }
+}
