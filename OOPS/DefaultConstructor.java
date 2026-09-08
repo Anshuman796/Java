57af95b2d295e629cf0cc9b0fef52981b41cce41
Anshuman Singh <gurujiofficial.2026@gmail.com>
2026-09-07 18:35:51 -0700

    Create DefaultConstructor.java
    Added Constructor.java

---
+++ OOPS/DefaultConstructor.java
@@ -0,0 +1,13 @@
+package OOPS;
+
+class Animal{
+    Animal(){
+        System.out.print("Constructor is Called");
+    }
+}
+public class DefaultConstructor {
+    public static void main(String[] args){
+        Animal obj = new Animal();
+    }
+     
+}
